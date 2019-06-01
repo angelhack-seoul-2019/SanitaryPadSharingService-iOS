ac713124c3d90867009b0b1be3f792b82c345f9d
tngusmiso <k1miso012@gmail.com>
2019-06-01 21:10:16 +0900

    Show Naver Map View

---
+++ Pods/Target Support Files/Pods-SharingService/Pods-SharingService-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SharingService : NSObject
+@end
+@implementation PodsDummy_Pods_SharingService
+@end
---
+++ SharingService/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  SharingService
+//
+//  Created by 임수현 on 01/06/2019.
+//  Copyright © 2019 AngelhackSeoul2019. All rights reserved.
+//
+
+import UIKit
+import NMapsMap
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var mapView: NMFMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
