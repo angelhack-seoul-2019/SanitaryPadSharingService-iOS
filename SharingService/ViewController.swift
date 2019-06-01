703268b64a4b839fc5b03058fe414e0acc20b792
tngusmiso <k1miso012@gmail.com>
2019-06-01 21:44:31 +0900

    Get User Current Location (GPS)

--- SharingService/ViewController.swift
+++ SharingService/ViewController.swift
@@ -11,13 +11,73 @@ import NMapsMap
 
 class ViewController: UIViewController {
 
+    // MARK: - instances
+    let locationManager = CLLocationManager()
+    let marker = NMFMarker()
+    
+    // MARK: - outlet
     @IBOutlet weak var mapView: NMFMapView!
     
+    // MARK: - life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setNaverMapDelegate()
     }
 
-
+    override func viewDidAppear(_ animated: Bool) {
+        updateLocation()
+    }
 }
 
+// MARK: - CLLocationManagerDelegate
+extension ViewController: CLLocationManagerDelegate {
+    
+    func updateLocation(){
+        self.locationManager.delegate = self                  // 뷰 컨트롤러를 로케이션 매니저 객체에 대한 앱델리게이트로 선언
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest  // 정확도 : 최고
+        self.locationManager.requestWhenInUseAuthorization()  // 위치 정보 권한 허가 요청
+        self.locationManager.startUpdatingLocation()          // 위치 정보 갱신 시작
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        //위치가 업데이트될때마다
+        if let coor = manager.location?.coordinate{
+            print("latitude" + String(coor.latitude) + "/ longitude" + String(coor.longitude))
+            
+            updateMarker(coor.latitude, coor.longitude)
+            updateCamera(coor.latitude, coor.longitude)
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager,didFailWithError error: Error) {
+        //에러 발생
+    }
+}
+
+// MARK: - NMFMapViewDelegate
+extension ViewController: NMFMapViewDelegate {
+    func setNaverMapDelegate(){
+        self.mapView.delegate = self
+    }
+    
+    // 마커 갱신
+    func updateMarker(_ latitude:CLLocationDegrees , _ longitude: CLLocationDegrees){
+        marker.position = NMGLatLng(lat: latitude, lng: longitude)
+        marker.mapView = mapView
+    }
+    
+    // 카메라 위치 갱신
+    func updateCamera(_ latitude:CLLocationDegrees , _ longitude: CLLocationDegrees) {
+        let cameraUpdate = NMFCameraUpdate(scrollTo: NMGLatLng(lat: latitude, lng: longitude), zoomTo: 16.0)
+        mapView.moveCamera(cameraUpdate)
+    }
+    func updateCamera(_ latitude:CLLocationDegrees , _ longitude: CLLocationDegrees, _ zoom: Double){
+        let cameraUpdate = NMFCameraUpdate(scrollTo: NMGLatLng(lat: latitude, lng: longitude), zoomTo: zoom)
+        mapView.moveCamera(cameraUpdate)
+    }
+    
+    // 지도 탭
+    func didTapMapView(_ point: CGPoint, latLng latlng: NMGLatLng) {
+        print("지도 탭 \(latlng.lat),\(latlng.lng)")
+    }
+}
